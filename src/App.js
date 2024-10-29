367d1baf96db4647aaf166c21fe3164990b90353
NozimjonShukrullayev <nozimshukrullayev@gmail.com>
2024-10-29 11:21:11 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="bg-black md:text-amber-400 md:bg-slate-400 text-red-700 text-2xl py-10 text-center font-bold underline">Hello world!</div>
+  )
 }
 
-export default App;
+export default App
